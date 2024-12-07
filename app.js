8baec4b3f4292addf7a32e34089003cdbd08d240
Samurai-Geo <geoloarox@gmail.com>
2024-12-07 14:24:40 -0300

    liberando cors domain

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index.js');
 const apiRouter = require('./routes/api.js');
 
 const app = express();
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
